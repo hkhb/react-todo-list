d74c443b5197d465ec5f82c9b2c4f2169f7bca0a
hkhb <h14hbk@gmail.com>
2025-05-12 15:55:31 +0900

    themaの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-//必要なコンポーネント
-//　リスト
-// import Lists from './components/lists';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider} from "@chakra-ui/react";
-import App from "./App.tsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+
+const theme = extendTheme({});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-     <ChakraProvider>
-       <App />
+    <ChakraProvider theme={theme}>
+      <App />
     </ChakraProvider>
-  </StrictMode>,
+  </StrictMode>
 );
